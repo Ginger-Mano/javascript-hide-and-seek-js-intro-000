4c6c6be9db896bd43206b8ad3452d609c68ec51a
imperative-publisher-3811 <mcasimirian@gmail.com>
2020-08-05 02:13:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ function getFirstSelector(selector) {
   return document.querySelector(selector); //returns the first selector that matches
   //must put return and semi-colon in order to work
 }
+
+function nestedTarget() {
+  return document.getTargetByName()
+}
